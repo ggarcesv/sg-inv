3fc7cb406c491f1fd215c9bf803b4ae0f0289a02
ggarcesv <garces1989@gmail.com>
2019-05-31 10:39:08 -0400

    Commit inicial

---
+++ TallerMecanico/Controllers/MarcasController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TallerMecanico.Controllers
+{
+    public class MarcasController : Controller
+    {
+        // GET: Marcas
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Ver()
+        {
+            return View();
+        }
+    }
+}
---
+++ TallerMecanico/Controllers/RepuestosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TallerMecanico.Controllers
+{
+    public class RepuestosController : Controller
+    {
+        // GET: Repuestos
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Ver()
+        {
+            return View();
+        }
+    }
+}
